95406ea79eabd503d8424e43f1ba1760c1535fb7
Güneş Balcı <gunesbalci66@hotmail.com>
2025-12-26 17:05:43 +0300

    Gravity is added to player. Player model changed.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,33 +3,34 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     CharacterController Controller;
-
     public float Speed;
-
     public Transform Cam;
-
+    private float Gravity;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-
         Controller = GetComponent<CharacterController>();
-
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
         float Horizontal = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
         float Vertical = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
-
+        Gravity -= 9.81f * Time.deltaTime;
+        
         Vector3 Movement = Cam.transform.right * Horizontal + Cam.transform.forward * Vertical;
-        Movement.y = 0f;
-
-
+        Movement.y = Gravity;
 
         Controller.Move(Movement);
+
+        if (Controller.isGrounded)
+        {
+            Gravity = 0f;
+        }
 
         if (Movement.magnitude != 0f)
         {
@@ -41,7 +42,13 @@ public class PlayerMovement : MonoBehaviour
             CamRotation.z = 0f;
 
             transform.rotation = Quaternion.Lerp(transform.rotation, CamRotation, 0.1f);
-
         }
+
+        if (animator != null)
+        {
+            Vector3 horizontalVel = Controller.velocity;
+            horizontalVel.y = 0f;
+            animator.SetBool("isRunning", horizontalVel.magnitude > 0.1f);
+        }       
     }
 }
