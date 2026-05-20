82e60599c6a20bbfb2fffe4eaa327a69451fa2e0
KuroiZZ <habiltatar01@gmail.com>
2026-05-20 13:12:42 +0300

    Enemy golem created and walk-attack-aoeattack animations created too.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -10,15 +11,25 @@ public class Enemy : MonoBehaviour
     private float Health;
     private bool alreadyAttacked;
     [SerializeField] private HealthBarUI healthBarUI;
+    [SerializeField] private GameObject AoeAttackObject;
+    private SphereCollider AoeHitBox;
+    [SerializeField] private float maxRadius = 4f;
+    [SerializeField] private float growDuration = 1f;
+    private Animator animator;
+    private bool isWalking;
 
     private void Awake()
     {
         Health = MaxHealth;
         alreadyAttacked = false;
+        animator = GetComponent<Animator>();
     }
 
     private void Start()
     {
+        AoeAttackObject.SetActive(false);
+        AoeHitBox = AoeAttackObject.GetComponent<SphereCollider>();
+        AoeHitBox.radius = 0f;
         healthBarUI.SetMaxHealth(MaxHealth);
         healthBarUI.SetHealth(Health);
     }
@@ -27,11 +38,26 @@ public class Enemy : MonoBehaviour
     {
         if(PlayerInAttackRange())
         {
-            Attack();
+            isWalking = false;
+            animator.SetBool("isWalking", isWalking);
+            int randomAttack = UnityEngine.Random.Range(0, 2);
+            Debug.Log(randomAttack);
+            if (randomAttack == 0)
+            {
+                Attack();
+            }
+            else
+            {
+                AOEAttack();
+            }
         }
         else
         {
-            ChasePlayer();
+            if(!alreadyAttacked)
+            {
+                ChasePlayer();
+            }
+            
         }
         RegenerateHealth();
     }
@@ -57,16 +83,65 @@ public class Enemy : MonoBehaviour
         if (!alreadyAttacked)
         {
             alreadyAttacked = true;
-            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+            animator.SetTrigger("isSwiped");
+            //Invoke(nameof(ResetAttack), timeBetweenAttacks);
 
             player.GetComponent<Player>().TakeDamageOrHeal(-Damage);
         }
     }
 
+    private void AOEAttack()
+    {
+        if (!alreadyAttacked)
+        {
+            AoeAttackObject.SetActive(true);
+            StartCoroutine(GrowHitbox());
+            alreadyAttacked = true;
+            animator.SetTrigger("isAOEAttacked");
+            //Invoke(nameof(ResetAttack), timeBetweenAttacks);
+
+            player.GetComponent<Player>().TakeDamageOrHeal(-Damage);
+        }
+    }
+
+     private IEnumerator GrowHitbox()
+    {
+        float timer = 0f;
+
+        AoeHitBox.radius = 0f;
+
+        while (timer < growDuration)
+        {
+            timer += Time.deltaTime;
+
+            float t = timer / growDuration;
+
+            AoeHitBox.radius = Mathf.Lerp(0f, maxRadius, t);
+
+            yield return null;
+        }
+
+        AoeHitBox.radius = maxRadius;
+
+        AoeHitBox.radius = 0f;
+
+        AoeAttackObject.SetActive(false);
+    }
+
     private void ChasePlayer()
     {
+        isWalking = true;
         Vector3 direction = (player.position - transform.position).normalized;
         transform.position += direction * MovementSpeed * Time.deltaTime;
+
+        Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
+
+        transform.rotation = Quaternion.RotateTowards(
+            transform.rotation,
+            targetRotation,
+            Time.deltaTime * 500f
+        );
+        animator.SetBool("isWalking", isWalking);
     }
 
     private bool PlayerInAttackRange()
